f0ecbc5bfa797d01d76f62a1d32bcae0b1e8b0c8
hansomer <sun_pro12580@163.com>
2017-03-28 15:15:05 +0800

    rebuild myserver

--- socket_programmer/socket_ThreadingTCPServer/sever_ThreadingTCPServer.py
+++ socket_programmer/socket_ThreadingTCPServer/sever_ThreadingTCPServer.py
@@ -5,29 +5,19 @@
 import SocketServer
 import time, socket, threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 监听端口:
-s.bind(('127.0.0.1', 9999))
-s.listen(9)
-print 'Waiting for connection...'
-while True:
-    # 接受一个新连接:
-    sock, addr = s.accept()
-    print 'Accept new connection from %s:%s...' % addr
-    sock.send('Welcome!')
-    while True:
-        data = sock.recv(1024)
-        time.sleep(1)
-        print 'recv msg is:'+data
-        #接收的为退出信息那么就退出。
-        if data == 'exit' or not data:
-            sock.close()
-            exit()
-        #接收的为非退出信息，那么就往客户端发送信息。
-        sock.send('Hello, %s!' % data)
-    sock.close()
-    print 'Connection from %s:%s closed.' % addr
-
+class MyServer(SocketServer.BaseRequestHandler):
+    def handle(self):
+        conn = self.request
+        conn.sendall('accept connect')
+        Flag = True
+        while Flag:
+            data = conn.recv(1024)
+            if data == 'exit':
+                print "server recv exit"
+                Flag = False
+            else:
+                print "server recv data %s"%data
+                conn.sendall('server recv data %s'%data)
 
 if __name__ == '__main__':
 	IP = '127.0.0.1'
