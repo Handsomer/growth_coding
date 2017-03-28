d4864caa9e7cad3acbd06cd293ba4f92557dfa4b
hansomer <sun_pro12580@163.com>
2017-03-28 11:05:15 +0800

    tcp stream socket

--- socket_programmer/simple_model/client_stream.py
+++ socket_programmer/simple_model/client_stream.py
@@ -10,7 +10,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 9999))
 # 接收欢迎消息:
 print s.recv(1024)
-for data in ['Michael', 'Tracy', 'Sarah']:
+for data in ['Michael', 'Tracy', 'exit']:
     # 发送数据:
     s.send(data)
     print s.recv(1024)
--- socket_programmer/simple_model/server_stream.py
+++ socket_programmer/simple_model/server_stream.py
@@ -3,18 +3,6 @@
 
 import time, socket, threading
 
-def tcplink(sock, addr):
-    print 'Accept new connection from %s:%s...' % addr
-    sock.send('Welcome!')
-    while True:
-        data = sock.recv(1024)
-        time.sleep(1)
-        if data == 'exit' or not data:
-            break
-        sock.send('Hello, %s!' % data)
-    sock.close()
-    print 'Connection from %s:%s closed.' % addr
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 监听端口:
 s.bind(('127.0.0.1', 9999))
@@ -23,6 +11,17 @@ print 'Waiting for connection...'
 while True:
     # 接受一个新连接:
     sock, addr = s.accept()
-    # 创建新线程来处理TCP连接:
-    t = threading.Thread(target=tcplink, args=(sock, addr))
-    t.start()
+    print 'Accept new connection from %s:%s...' % addr
+    sock.send('Welcome!')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        print 'recv msg is:'+data
+        #接收的为退出信息那么就退出。
+        if data == 'exit' or not data:
+            sock.close()
+            exit()
+        #接收的为非退出信息，那么就往客户端发送信息。
+        sock.send('Hello, %s!' % data)
+        sock.close()
+    print 'Connection from %s:%s closed.' % addr
