2083f7d2241493c9d1f0515718f8678811bb859c
SW <sun_pro@12580@163.com>
2018-01-28 13:12:09 +0800

    添加装饰器的修改

--- colligate/decorator.py
+++ colligate/decorator.py
@@ -1,6 +1,43 @@
-import functools
+#coding=utf-8
+# import functools
 
 
+
+def out(func):
+    def add(*args,**kwargs):
+        print "原有功能"
+        r = func(*args,**kwargs)
+        print "后增功能"
+        return r
+    return add
+
+# 装饰器是一个函数
+#不需要修改调用方式
+#
+def out1(func):
+    def add(*args,**kwargs):
+        print "start"
+        r = func(*args,**kwargs)
+        print "end"
+        return r
+    return add
+
+'''
+此处@out 相当于
+# n = out(f)
+# f = n
+
+'''
+@out1
+@out
+def f(a,b):
+    print "xianyougongne",a,b
+
+
+# n = out(f)
+# f = n
+f(1,2)
+'''
 def run_time(func):
 	#修改生成器函数的名称
 	#相当于wrapper.__name__ = func.__name__ 
@@ -15,4 +52,4 @@ def f_tmp():
 	print("run f_tmp")
 
 f_tmp()
-print(f_tmp.__name__)
+print(f_tmp.__name__)'''
