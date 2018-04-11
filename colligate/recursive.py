73d1623eb9198383f0a8cce4146b3e7fa775319d
sun_wei <406669201@qq.com>
2018-04-11 15:24:59 +0800

    代码规范

--- colligate/recursive.py
+++ colligate/recursive.py
@@ -4,6 +4,10 @@ def mysum(L):
     first,*rest = L
     return first if not rest else first + mysum(rest)
 
-# a = [1,2,3]
-a =['1','2']
-print(mysum(a))
+def iterator_way():
+    print(sum(i for i in range(10)))# 这种写法等效于下面
+
+if __name__ == "__main__":
+    # a = [1,2,3]
+    a =['1','2']
+    print(mysum(a))
