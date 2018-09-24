e52a7afc52767e96f993d7212df185d18bd866d5
Handsomer <sun_pro12580@163.com>
2018-09-24 11:08:55 +0800

    动态规划实现获取最大差值（后面数据大于前面的数据）

--- alg_py/leetcode_alg.py
+++ alg_py/leetcode_alg.py
@@ -10,5 +10,17 @@ class ac_303:
 		return sum(self.nums[i: j+1]) if j < len(self.nums) else sum(self.nums[i:])
 
 
+class ac_121:
+    def maxProfit(self, prices):
+        import sys
+        max_profit, min_price = 0, sys.maxsize
+        for per_price in prices:
+            min_price = min(min_price, per_price)
+            max_profit = max(max_profit, per_price-min_price)
+        return max_profit
+
+
 if __name__ == '__main__':
-	pass
+    ac_obj = ac_121()
+
+    print(ac_obj.maxProfit([7,1,5,3,6,4]))
