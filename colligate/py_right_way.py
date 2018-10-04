84aa68d0cdff88e790194b246d89ae458fe28287
Handsomer <sun_pro12580@163.com>
2018-10-04 14:35:27 +0800

    python 中的一切都是对象

--- colligate/py_right_way.py
+++ colligate/py_right_way.py
@@ -20,3 +20,41 @@ def function_right(l=None):
 print(function_right())
 print(function_right())
 print(function_right())
+
+#一切都是对象
+# bad
+def print_addition_table():
+    for x in range(1, 3):
+        for y in range(1, 3):
+            print(str(x + y) + '\n')
+
+def print_subtraction_table():
+    for x in range(1, 3):
+        for y in range(1, 3):
+            print(str(x - y) + '\n')
+
+def print_multiplication_table():
+        for x in range(1, 3):
+            for y in range(1, 3):
+                print(str(x * y) + '\n')
+
+def print_division_table():
+    for x in range(1, 3):
+        for y in range(1, 3):
+            print(str(x / y) + '\n')
+
+print_addition_table()
+print_subtraction_table()
+print_multiplication_table()
+print_division_table()
+
+# good, python一切都是对象，可以函数作为参数，类似技巧可以用来简化代码
+import operator as op
+
+def print_table(operator):
+    for x in range(1, 3):
+        for y in range(1, 3):
+            print(str(operator(x, y)) + '\n')
+
+for operator in (op.add, op.sub, op.mul, op.div):
+    print_table(operator)
