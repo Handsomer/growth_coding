59799a55e577e9b686d835b8e758853e53451294
Handsomer <sun_pro12580@163.com>
2018-10-13 12:04:09 +0800

    动态规划：实现爬楼梯的方法数

--- alg_py/leetcode_alg.py
+++ alg_py/leetcode_alg.py
@@ -19,8 +19,40 @@ class ac_121:
             max_profit = max(max_profit, per_price-min_price)
         return max_profit
 
+class ac_70_outTime:
+    def climbStairs(self, n):
+        """
+           :type n: int
+           :rtype: int
+        """
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+class ac_70:
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        elif n ==2:
+            return 2
+        first = 1
+        second = 2
+        for i in range(n-2):
+            ret = first + second
+            first = second
+            second = ret
+        return ret
+
 
 if __name__ == '__main__':
-    ac_obj = ac_121()
+    """
+        ac_obj = ac_121()
 
-    print(ac_obj.maxProfit([7,1,5,3,6,4]))
+        print(ac_obj.maxProfit([7,1,5,3,6,4]))
+        ac_obj = ac_70(4)
+    """
+    ac_obj = ac_70()
+    print(ac_obj.climbStairs(4))
