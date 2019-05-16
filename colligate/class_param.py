7decc2242aa54447160393d3468c698d12ac6d1c
Handsomer <sun_pro12580@163.com>
2019-05-16 11:20:32 +0800

    类成员变量，与对象的变量

---
+++ colligate/class_param.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+
+#类变量
+class Test(object):
+    #定义类变量
+    num_of_instance = 0
+    #定义成员变量, 不可变对象
+    instance_param_stubborn = 'aaa'
+    #定义成员变量, 可变对象
+
+    def __init__(self, name):
+        self.name = name
+        Test.num_of_instance += 1
+    
+
+if __name__ == "__main__":
+    print Test.num_of_instance
+    t1 = Test('Jack')
+    t1.instance_param_stubborn = 'bbb'
+    print t1.name, t1.num_of_instance, Test.num_of_instance,t1.instance_param_stubborn
+    t2 = Test('lucy')
+    print t2.name, t2.num_of_instance,Test.num_of_instance, t2.instance_param_stubborn
+
