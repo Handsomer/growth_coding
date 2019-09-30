1a24f9a3db212b5cfaea0695886a04f049900f15
Handsomer <sun_pro12580@163.com>
2019-09-30 19:52:08 +0800

    使用collections的Counte功能

--- collection_use.py
+++ collection_use.py
@@ -6,3 +6,12 @@ PointModule = collections.namedtuple('Point','x,y')
 
 p = PointModule(1,2)
 print(p.x, p.y, p[0], p[1])
+
+print('------ -------')
+counter_obj = collections.Counter('aaaabfbbbbb')
+print(counter_obj['a'])
+print(counter_obj.most_common(3))
+
+print('------ -------')
+counter_obj = collections.Counter([12,1,1,1,4,4,6,6,6])
+print(counter_obj.most_common(3))
