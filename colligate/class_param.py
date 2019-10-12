a8aaa9c043905690ff6fe4950d234fdbb696361f
Handsomer <sun_pro12580@163.com>
2019-10-12 14:45:12 +0800

    添加类成员，可变对象的调用

--- colligate/class_param.py
+++ colligate/class_param.py
@@ -7,6 +7,7 @@ class Test(object):
     #定义成员变量, 不可变对象
     instance_param_stubborn = 'aaa'
     #定义成员变量, 可变对象
+    instance_param_unstubborn = [1]
 
     def __init__(self, name):
         self.name = name
@@ -14,10 +15,12 @@ class Test(object):
     
 
 if __name__ == "__main__":
-    print Test.num_of_instance
+    print(Test.num_of_instance)
     t1 = Test('Jack')
     t1.instance_param_stubborn = 'bbb'
-    print t1.name, t1.num_of_instance, Test.num_of_instance,t1.instance_param_stubborn
+    print(t1.name, t1.num_of_instance, Test.num_of_instance,t1.instance_param_stubborn, Test.instance_param_stubborn)
+    t1.instance_param_unstubborn.append(2)
+    print(t1.instance_param_unstubborn, Test.instance_param_unstubborn)
     t2 = Test('lucy')
-    print t2.name, t2.num_of_instance,Test.num_of_instance, t2.instance_param_stubborn
-
+    print(t2.name, t2.num_of_instance,Test.num_of_instance, t2.instance_param_stubborn, Test.instance_param_stubborn)
+    print(t2.instance_param_unstubborn, Test.instance_param_unstubborn)
